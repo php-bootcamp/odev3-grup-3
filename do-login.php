9a59d0f077bf41f7899e2b2918878444dad23407
Azad Furkan ŞAKAR <75483091+afsakar@users.noreply.github.com>
2021-02-09 23:48:10 +0300

    do-login
    ödev-3 do-login.php

--- do-login.php
+++ do-login.php
@@ -23,3 +23,36 @@
   * geri geldiğimde hata mesajı ile birlikte formda sadece kullanıcı adı kısmının
   * `eray` ile dolmasını bekliyoruz.)
   */
+if (!isset($_SESSION)) {
+	session_start();
+}
+
+if ( isset($_POST['kullanici']) || isset($_POST['parola']) ) {
+
+	if ( !isset($_POST['kullanici']) ) {
+		header("Location: login.php?durum=0");
+	}
+
+	if ( !isset($_POST['parola']) ) {
+		header("Location: login.php?durum=0");
+	}
+
+	$kullanici = $_POST['kullanici'];
+	$parola = $_POST['parola'];
+
+	$_SESSION['kullanici'] = $kullanici;
+
+	if ( $kullanici == 'bootcamp' && $parola == 'bootcamp' ) {
+		$_SESSION['giris'] = '1';
+		header("Location: posts.php");
+	}
+	else {
+		header("Location: login.php?durum=0");
+	}
+
+}
+else {
+	header("Location: login.php?durum=0");
+}
+?>
+
