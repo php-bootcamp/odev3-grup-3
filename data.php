794a4c5786c9e1941f5095db3781531985af40c3
mchtylmz <35258432+mchtylmz@users.noreply.github.com>
2021-02-09 23:48:36 +0300

    data

--- data.php
+++ data.php
@@ -4,11 +4,13 @@
  * dosya isimlerini değiştirebilir veya buradaki işlemleri farklı dosyalara
  * bölebilirsiniz.
  */
+
  /**
   * Uygulamanın çeşitli sayfalarında PHP Bootcamp içerisinde işlediğimiz
   * konulara dair ufak makalelerin veya lorem ipsum gibi yer alması bekleniyor.
   * **Not:** Henüz veritabanı görmediğimiz için bu yazıları sabit (hardcoded) verebilirsiniz.
   */
+
 $yazilar = array(
 	array(
 		'title' => 'DNS Nedir?',
