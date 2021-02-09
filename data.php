e360fb8fc6f438df04f1aee06aee6d9e6341a67d
mchtylmz <35258432+mchtylmz@users.noreply.github.com>
2021-02-09 23:47:39 +0300

    data - Mücahit
    grup 3 data

--- data.php
+++ data.php
@@ -1,18 +1,14 @@
 <?php
-
 /**
  * Bu dosya örnek amaçlı olarak oluşturulmuştur. Grupla beraber karar verip
  * dosya isimlerini değiştirebilir veya buradaki işlemleri farklı dosyalara
  * bölebilirsiniz.
  */
-
  /**
   * Uygulamanın çeşitli sayfalarında PHP Bootcamp içerisinde işlediğimiz
   * konulara dair ufak makalelerin veya lorem ipsum gibi yer alması bekleniyor.
   * **Not:** Henüz veritabanı görmediğimiz için bu yazıları sabit (hardcoded) verebilirsiniz.
   */
-
-
 $yazilar = array(
 	array(
 		'title' => 'DNS Nedir?',
