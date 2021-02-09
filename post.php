a496d1def4b449a5a750549e222f2ad23238cad9
mehmetsinan ozdemir <73998522+mehmetsinanozdemir@users.noreply.github.com>
2021-02-09 23:56:20 +0300

    Update post.php

--- post.php
+++ post.php
@@ -1,24 +1,53 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>...</title>
-</head>
-<body>
-    <?php
-    
-    /**
-     * Bu dosya örnek amaçlı olarak oluşturulmuştur. Grupla beraber karar verip
-     * dosya isimlerini değiştirebilir veya buradaki işlemleri farklı dosyalara
-     * bölebilirsiniz.
-     */
+<?php
+require __DIR__ . '/functions.php';
 
-    /**
-     * **Giriş yapmış kullanıcının** `posts.php` veya `seen.php` sayfaları üzerinden
-     * yönlenip **sabit** oluşturulmuş **tek bir** yazıya dair detay bilgisine görebileceği
-     * sayfadır. Kullanıcının bu yazıyı okuduğu kaydedilmelidir.
-     */
-    
-    ?>
+if (!isset($_GET['id'])) {
+	header("Location: posts.php");
+}
+$key = $_GET['id'];
+if ( !isset($yazilar[$key]) ) {
+	header("Location: posts.php");
+}
+set_cookie('key' . $key, 'okundu');
+
+$yazi = $yazilar[$key];
+?>
+<!DOCTYPE html>
+<html lang="tr">
+	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title><?php echo $yazi['title']; ?></title>
+	<link rel="stylesheet" href="style.css">
+<body class="yazilar-sayfa">
+
+	<h2 style="text-align: center">
+		Hoş geldin, <?php echo $_SESSION['kullanici']; ?>
+	</h2>
+	
+	<div class="menu">
+		<h3 class="baslik"><?php echo $yazi['title']; ?></h3>
+		<div style="display: flex">
+			<a href="posts.php" class="okunanlar" style="margin: 0 5px;">Tüm Yazılar</a>
+			<form action="seen-delete.php" method="post" style="margin: 0 5px;">
+				<input type="hidden" name="yazi_key" value="<?=$key?>">
+				<input type="hidden" name="location" value="posts.php">
+				<button type="submit" class="okunanlar">Okunanlardan Kaldır</button>
+			</form>
+		</div>
+	</div>
+
+	<div class="yazilar">
+
+		<div class="yazi">
+			<img src="<?php echo $yazi['image']; ?>" class="resim">
+			<div class="icerik">
+				<p><?php echo $yazi['brief']; ?></p>
+			</div>
+		</div>
+		<div class="uzun-icerik">
+			<p><?php echo $yazi['content']; ?></p>
+		</div>
+
+	</div>
 </body>
 </html>
