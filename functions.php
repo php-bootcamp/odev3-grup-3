b9cb42c90a86db93fd7b792e1d7784f939c84235
FurkanKarabacak <78212309+FurkanKarabacak@users.noreply.github.com>
2021-02-09 23:53:26 +0300

    Update functions.php
    function.php

--- functions.php
+++ functions.php
@@ -1,4 +1,31 @@
 <?php
+if (!isset($_SESSION)) {
+	session_start();
+}
+if ( !isset($_SESSION['giris']) || $_SESSION['giris'] != '1') {
+	header("Location: login.php");
+}
+require __DIR__ . '/data.php';
+
+function set_cookie($isim, $deger)
+{
+	setcookie($isim, $deger, time() + 3600);
+}
+
+function get_cookie($isim)
+{
+	if ( isset($_COOKIE[$isim]) ) {
+		return $_COOKIE[$isim];
+	} else {
+		return false;
+	}
+}
+
+function delete_cookie($isim)
+{
+	setcookie($isim, '', time() - 3600);
+}
+?>
 
 /**
  * Bu dosya örnek amaçlı olarak oluşturulmuştur. Grupla beraber karar verip
