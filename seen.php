cb464c943f190322690dc9aead73b84b2f0be5a0
fk55 <76855467+fk55@users.noreply.github.com>
2021-02-09 23:55:43 +0300

    Update seen.php

--- seen.php
+++ seen.php
@@ -1,24 +1,44 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>...</title>
-</head>
-<body>
-    <?php
-    
-    /**
-     * Bu dosya örnek amaçlı olarak oluşturulmuştur. Grupla beraber karar verip
-     * dosya isimlerini değiştirebilir veya buradaki işlemleri farklı dosyalara
-     * bölebilirsiniz.
-     */
+<?php
+require __DIR__ . '/functions.php';
 
-    /**
-     * **Giriş yapmış kullanıcının** uygulama üzerinde okumuş olduğu yazıların
-     * listelendiği sayfadır. `posts.php` sayfasında olduğu gibi yazı hakkında ufak
-     * bilgi ve detay linki yer almalıdır.
-     */
-    
-    ?>
+?>
+<!DOCTYPE html>
+<html lang="tr">
+	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Okunanlar</title>
+	<link rel="stylesheet" href="style.css">
+<body class="yazilar-sayfa">
+
+	<h2 style="text-align: center">
+		Hoş geldin, <?php echo $_SESSION['kullanici']; ?>
+	</h2>
+
+	<div class="menu">
+		<h3 class="baslik">Okunanlar</h3>
+		<a href="posts.php" class="okunanlar">Tüm Yazılar</a>
+	</div>
+
+	<div class="yazilar">
+		<?php foreach ($yazilar as $key => $okunan): ?>
+			<?php if (get_cookie('key' . $key) != false): ?>
+				<div class="yazi">
+					<img src="<?php echo $okunan['image']; ?>" class="resim">
+					<div class="icerik">
+						<h3><?php echo $okunan['title']; ?></h3>
+						<p><?php echo $okunan['brief']; ?></p>
+						<div class="islem">
+							<form action="seen-delete.php" method="post">
+								<input type="hidden" name="yazi_key" value="<?=$key?>">
+								<button type="submit" class="kaldir">Kaldır</button>
+							</form>
+							<a href="post.php?id=<?=$key?>" class="detay">Detay</a>
+						</div>
+					</div>
+				</div>
+			<?php endif; ?>
+		<?php endforeach; ?>
+
+	</div>
 </body>
 </html>
